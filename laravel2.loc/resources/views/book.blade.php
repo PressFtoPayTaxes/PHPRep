b3780a774229d8deeb7790b03c3e343fc4630394
Игорь <igor_2002_25@mail.ru>
2020-03-02 21:31:22 +0600

    Delete

--- laravel2.loc/resources/views/book.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container w-25">
-        @component('components.book_card', ['book' => $book])
-            {{ $book->description }}
-        @endcomponent
-    </div>
-@endsection
